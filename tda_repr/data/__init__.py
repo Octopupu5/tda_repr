1dafafff543c1c643d2d7bb789bd1bfe900ae9aa
Octopupu5 <pankratovstepan2004@gmail.com>
2026-03-19 09:40:00 +0300

    Add public API, data, CLI, and viz

---
+++ tda_repr/data/__init__.py
@@ -0,0 +1,11 @@
+from .registry import DataBundle, get_dataset, make_dataloaders, preview_cv_samples
+
+__all__ = [
+	"DataBundle",
+	"get_dataset",
+	"make_dataloaders",
+	"preview_cv_samples",
+]
+
+
+
---
+++ tda_repr/viz/__init__.py
@@ -0,0 +1,18 @@
+from .runlog import (
+	list_run_dirs,
+	load_epoch_end_records,
+	list_benchmarks,
+	list_repr_layers,
+	list_scalar_series_keys,
+	get_series,
+)
+
+__all__ = [
+	"list_run_dirs",
+	"load_epoch_end_records",
+	"list_benchmarks",
+	"list_repr_layers",
+	"list_scalar_series_keys",
+	"get_series",
+]
+
